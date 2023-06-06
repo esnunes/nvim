e0358900d79f290cb194107bb9d43530080d5eb9
Eduardo Nunes <esnunes@gmail.com>
2023-06-06 16:15:29 -0300

    add support to swift lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -21,3 +21,16 @@ end)
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
 lsp.setup()
+
+-- Swift SourceKit LSP
+local sourcekit_lsp_path = vim.fn.system('xcrun -f sourcekit-lsp'):gsub('[\n\r]', '')
+require('lspconfig.configs').sourcekit_lsp = {
+    default_config = {
+        name = 'sourcekit-lsp',
+        cmd = { sourcekit_lsp_path },
+        filetypes = { 'swift' },
+        root_dir = require('lspconfig.util').root_pattern('.git', '*.xcodeproj', '*.swift'),
+    }
+}
+
+require('lspconfig').sourcekit_lsp.setup({})
