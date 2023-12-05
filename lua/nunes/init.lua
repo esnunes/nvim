37f3dfa06738438e3379fca767d1fb3f2bc45ce4
Eduardo Nunes <esnunes@gmail.com>
2023-12-05 00:25:41 -0300

    force go organize imports prior to save

--- lua/nunes/init.lua
+++ lua/nunes/init.lua
@@ -1,6 +1,13 @@
 require('nunes.set')
 require('nunes.remap')
 
+vim.api.nvim_create_autocmd('BufWritePre', {
+    pattern = '*.go',
+    callback = function()
+        vim.lsp.buf.code_action({ context = { only = { 'source.organizeImports' } }, apply = true })
+    end
+})
+
 vim.filetype.add({
     extension = {
         templ = "templ",
