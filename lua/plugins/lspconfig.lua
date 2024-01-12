c708d844c67851b944b00db5be284a41dc3ac16b
Eduardo Nunes <esnunes@gmail.com>
2024-01-12 15:49:19 -0300

    migrate to lazy

---
+++ lua/plugins/lspconfig.lua
@@ -0,0 +1,66 @@
+return {
+  "neovim/nvim-lspconfig",
+
+  dependencies = {
+    "folke/neodev.nvim",
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "hrsh7th/nvim-cmp",
+  },
+
+  opts = nil,
+
+  config = function()
+    require("neodev").setup({})
+    require("mason").setup()
+
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+    require("mason-lspconfig").setup({
+      ensure_installed = { "lua_ls", "gopls", "templ" },
+
+      handlers = {
+        function(server_name)
+          require("lspconfig")[server_name].setup({
+            capabilities = capabilities,
+          })
+        end,
+
+        ["lua_ls"] = function()
+          require("lspconfig").lua_ls.setup({
+            capabilities = capabilities,
+            settings = {
+              Lua = {
+                diagnostics = {
+                  globals = { "vim" },
+                },
+              },
+            },
+          })
+        end,
+
+        ["templ"] = function()
+          local opts = {}
+
+          -- use local version of templ lsp if possible
+          local cmd_path = vim.loop.fs_realpath(vim.fn.getcwd() .. "/.bin/templ")
+          if cmd_path ~= nil and vim.fn.filereadable(cmd_path) then
+            opts.cmd = { cmd_path, "lsp" }
+          end
+
+          require("lspconfig").templ.setup(opts)
+        end
+      },
+    })
+
+    vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
+      vim.lsp.handlers.hover,
+      { border = 'rounded' }
+    )
+
+    vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
+      vim.lsp.handlers.signature_help,
+      { border = 'rounded' }
+    )
+  end,
+}
